a977394ddd8733e165807ed339f04597df0cb12c
Fengyuan Wu <wfy0425@yeah.net>
2021-02-08 11:59:41 +0800

    section 25

--- src/components/Scream.js
+++ src/components/Scream.js
@@ -5,6 +5,7 @@ import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
 import PropTypes from 'prop-types'
 import MyButton from '../util/MyButton'
+import DeleteScream from './DeleteScream.js'
 
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
@@ -19,6 +20,7 @@ import { connect } from 'react-redux'
 import { likeScream, unlikeScream } from '../redux/actions/dataActions'
 const styles = {
 	card: {
+		position: 'relative',
 		display: 'flex',
 		marginBottom: 20,
 	},
@@ -60,7 +62,10 @@ class Scream extends Component {
 				likeCount,
 				commentCount,
 			},
-			user: { authenticated },
+			user: {
+				authenticated,
+				credentials: { handle },
+			},
 		} = this.props
 		const likeButton = !authenticated ? (
 			<Link to="/login">
@@ -77,6 +82,10 @@ class Scream extends Component {
 				<FavoriteBorder color="primary" />
 			</MyButton>
 		)
+		const deleteButton =
+			authenticated && userHandle === handle ? (
+				<DeleteScream screamId={screamId} />
+			) : null
 		return (
 			<Card className={classes.card}>
 				<CardMedia
@@ -93,6 +102,7 @@ class Scream extends Component {
 					>
 						{userHandle}
 					</Typography>
+					{deleteButton}
 					<Typography variant="body2" color="textSecondary">
 						{dayjs(createdAt).fromNow()}
 					</Typography>
