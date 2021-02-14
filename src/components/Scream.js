e55c054461b46134f6832b8b320d5ca7bab3a3d5
Fengyuan Wu <wfy0425@yeah.net>
2021-02-14 11:00:48 +0800

    section 27

--- src/components/Scream.js
+++ src/components/Scream.js
@@ -6,6 +6,7 @@ import relativeTime from 'dayjs/plugin/relativeTime'
 import PropTypes from 'prop-types'
 import MyButton from '../util/MyButton'
 import DeleteScream from './DeleteScream.js'
+import ScreamDialog from './ScreamDialog.js'
 
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
@@ -113,6 +114,7 @@ class Scream extends Component {
 						<ChatIcon color="primary" />
 					</MyButton>
 					<span>{commentCount} comments</span>
+					<ScreamDialog screamId={screamId} userHandle={userHandle} />
 				</CardContent>
 			</Card>
 		)
